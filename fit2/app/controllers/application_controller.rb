c712edf65b9cba5502fd530c02b2b8a80b55c64a
Natasha Clark <Natasharclark@gmail.com>
2017-03-02 11:15:20 -0500

    CRUD AND CREATE WORKING AGAIN

--- fit2/app/controllers/application_controller.rb
+++ fit2/app/controllers/application_controller.rb
@@ -4,11 +4,5 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!, :except => [ :homepage, :splash]
 
-  def after_sign_in_path_for(resource)
-    homepage_path
-  end
 
-  def after_sign_up_path_for(resource)
-    new_user_profile_path
-  end
 end
