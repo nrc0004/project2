c2b6a55c32b2eb19464f1952a039dd866838b15f
Natasha Clark <Natasharclark@gmail.com>
2017-03-06 14:11:16 -0500

    add optional true to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :programs
-  has_one :profile
+  has_one :profile, optional: true
 
   # after_initialize :create_profile
   #   def create_profile
